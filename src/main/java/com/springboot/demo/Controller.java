7036e71ddfbe22455cadd75a5b12862224d50b9a
Arnab Samanta <samanta_arnab2006@yahoo.co.in>
2018-11-22 12:14:49 +0000

    initial commit for test

---
+++ src/main/java/com/springboot/demo/Controller.java
@@ -0,0 +1,18 @@
+package com.springboot.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	
+	
+	@GetMapping(value="health")
+	public String healthCheck() {
+		return "Health is OK";
+		
+		
+	}
+
+}
