84fb50d455e18858672d8edb8d6b4e97406ee958
Arnab Samanta <samanta_arnab2006@yahoo.co.in>
2018-11-30 18:48:19 +0000

    2nd change to detect
    2nd change to detect

--- src/main/java/com/springboot/demo/Controller.java
+++ src/main/java/com/springboot/demo/Controller.java
@@ -10,7 +10,7 @@ public class Controller {
 	
 	@GetMapping(value="health")
 	public String healthCheck() {
-		return "Health is OK. This is change for docker change";
+		return "Health is OK. 2nd change";
 		
 		
 	}
